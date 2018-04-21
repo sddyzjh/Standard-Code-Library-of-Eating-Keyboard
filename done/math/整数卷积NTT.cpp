d4dafedeeb77f44c13a8a14e60ffeb3cf5e02326
sddyzjh <463722952@qq.com>
2018-04-22 07:22:03 +0800

    Add files via upload

--- done/math/整数卷积NTT.cpp
+++ done/math/整数卷积NTT.cpp
@@ -1,10 +1,25 @@
 /*
 	计算形式为a[n] = sigma(b[n-i]*c[i])的卷积,结果存在c中
 	下标从0开始
-	调用juanji(n,b,c)
-	P为模数
-	G是P的原根
+	调用convolution(a,n,b,m,c)
+	MOD为模数,CRT合并
 */
+const int MOD=1000003,m1=998244353,m2=1004535809;
+const ll P=1002772198720536577LL;
+inline ll mul(ll a,ll b){
+	ll d=(ll)floor(a*(double)b/P+0.5);
+	ll ret=a*b-d*P;
+	if(ret<0)ret+=P;
+	return ret;
+}
+inline int CRT(int r1,int r2){
+	ll a = mul(r1,m2);
+	a = mul(a,332747959ll);
+	ll b = mul(r2,m1);
+	b = mul(b,669690699ll);
+	a = (a+b)%P;
+	return a%MOD;
+}
 const ll P=998244353;
 const ll G=3;
 void change(ll y[],int n){
@@ -19,11 +34,11 @@ void change(ll y[],int n){
 		j+=k;
 	}
 }
-void NTT_(ll y[],int len,int on){
+void NTT(ll y[],int len,int on){
 	change(y,len);
 	for(int h=2;h<=len;h<<=1){
-		ll wh=powm(G,(P-1)/h,P);
-		if(on<0)wh=powm(wh,P-2,P);
+		ll wh=powM(G,(P-1)/h,P);
+		if(on<0)wh=powM(wh,P-2,P);
 		for(int i=0;i<len;i+=h){
 			ll w=1;
 			int r=h>>1;
@@ -39,17 +54,41 @@ void NTT_(ll y[],int len,int on){
 		}
 	}
 	if(on<0){
-		ll I=powm((ll)len,P-2,P);
-		Rep(i,len)y[i]=y[i]*I%P;
+		ll I=powM((ll)len,P-2,P);
+		rep(i,len)y[i]=y[i]*I%P;
 	}
 }
-void juanji(int n, ll *b, ll *c){
-	int len=1;
-	while(len<(n<<1))len<<=1;
-	Repab(i,n,len)c[i]= b[i] = 0;
-	NTT_(b,len,1);
-	NTT_(c,len,1);
-	Rep(i,len)
-		c[i]= c[i]*b[i]%P;
-	NTT_(c,len,-1);
+ll x1[N], x2[N], x3[N], x4[N];
+void convolution(ll a[], int l1, ll b[], int l2, ll c[]){
+	int len = 1;
+	while(len < l1*2 || len < l2*2)len <<= 1;
+	rep(i,l1)
+		x1[i] = a[i];
+	repab(i,l1,len)
+		x1[i] = 0;
+	rep(i,l2)
+		x2[i] = b[i];
+	repab(i,l2,len)
+		x2[i] = 0;
+	NTT(x1,len,1,m1);
+	NTT(x2,len,1,m1);
+	rep(i,len)
+		x3[i] = x1[i]*x2[i]%m1;
+	NTT(x3,len,-1,m1);
+		
+	rep(i,l1)
+		x1[i] = a[i];
+	repab(i,l1,len)
+		x1[i] = 0;
+	rep(i,l2)
+		x2[i] = b[i];
+	repab(i,l2,len)
+		x2[i] = 0;
+	NTT(x1,len,1,m2);
+	NTT(x2,len,1,m2);
+	rep(i,len)
+		x4[i] = x1[i]*x2[i]%m2;
+	NTT(x4,len,-1,m2);
+	rep(i,len)
+		c[i] = CRT(x3[i], x4[i]);
 }
