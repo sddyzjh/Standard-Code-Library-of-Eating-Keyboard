7da94096a71dcae6378d560525e44f1e9ef774e5
sddyzjh <463722952@qq.com>
2018-11-14 09:29:39 +0800

    Update template

--- done/others/template.cpp
+++ done/others/template.cpp
@@ -6,24 +6,29 @@ typedef double db;
 typedef long double ld;
 typedef long long ll;
 typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef set<int> si;
 
-void sc(int &x){scanf("%d", &x);}
-void sc(uint &x){scanf("%u", &x);}
-void sc(ll &x){scanf("%lld", &x);}
-void sc(ull &x){scanf("%llu", &x);}
-void sc(db &x){scanf("%lf", &x);}
-void sc(ld &x){scanf("%Lf", &x);}
-void sc(char *x){scanf("%s", x);}
-void sc(char &x){scanf("%c", &x);}
+int sc(int &x){return scanf("%d", &x);}
+int sc(uint &x){return scanf("%u", &x);}
+int sc(ll &x){return scanf("%lld", &x);}
+int sc(ull &x){return scanf("%llu", &x);}
+int sc(db &x){return scanf("%lf", &x);}
+int sc(ld &x){return scanf("%Lf", &x);}
+int sc(char *x){return scanf("%s", x);}
+int sc(char &x){return scanf("%c", &x);}
+template<typename T, typename... Args> void sc(T& v, Args&... args) { sc(v); sc(args...); }
 
 void pr(const int &x){printf("%d", x);}
 void pr(const uint &x){printf("%u", x);}
 void pr(const ll &x){printf("%lld", x);}
 void pr(const ull &x){printf("%llu", x);}
-void pr(const db &x){printf("%lf", x);}
-void pr(const ld &x){printf("%Lf", x);}
+void pr(const db &x){printf("%.10lf", x);}
+void pr(const ld &x){printf("%.10Lf", x);}
 void pr(const char *x){printf("%s", x);}
 void pr(const char &x){printf("%c", x);}
+template<typename T, typename... Args> void pr(T const& v, Args const& ... args) { pr(v); pr(' '); pr(args...); }
 
 #define sp pr(" ")
 #define ln pr("\n")
@@ -50,40 +55,40 @@ void pr(const char &x){printf("%c", x);}
 #define ALL(x) (x).begin(), (x).end()
 #define SZ(x) ((int)(x).size())
 
-#define for_each_edge(u) for(int i = point[u];i;i=G[i].n)
-
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+#define for_edge(u) for(int i = point[u];i;i = G[i].n)
 
 const int N = 100010;
 const int M = 1024;
-const int MOD = 1000000007;
-void _add(int &a, int b){a = (a+b)%MOD;}
-void _sub(int &a, int b){a = (a+MOD-b)%MOD;}
-void _mul(int &a, int b){a = (ll)a*b%MOD;}
+const int mod = 1000000007;
+inline void _add(int &a, int b){a = (a+b)%mod;}
+inline void _sub(int &a, int b){a = (a+mod-b)%mod;}
+inline void _mul(int &a, int b){a = (ll)a*b%mod;}
 
-int _Add(int a, int b){return (a+b)%MOD;}
-int _Sub(int a, int b){return (a+MOD-b)%MOD;}
-int _Mul(int a, int b){return (ll)a*b%MOD;}
+inline int _Add(int a, int b){return (a+b)%mod;}
+inline int _Sub(int a, int b){return (a+mod-b)%mod;}
+inline int _Mul(int a, int b){return (ll)a*b%mod;}
 
-void _max(int &a, int b){a = max(a,b);}
-void _min(int &a, int b){a = min(a,b);}
+template<typename T>inline void _max(T &a, T b){if(b > a)a = b;}
+template<typename T>inline void _min(T &a, T b){if(b < a)a = b;}
 
 #define gcd __gcd
 
-ll pw(ll a, ll b){ll res(1);while(b){if(b&1)res=res*a%MOD;a=a*a%MOD;b>>=1;}return res;}
-ll pwM(ll a, ll b, ll mod){ll res(1);while(b){if(b&1)res=res*a%mod;a=a*a%mod;b>>=1;}return res;}
+int pw(int a, int b){int res(1);while(b){if(b&1)_mul(res,a);_mul(a,a);b>>=1;}return res;}
+int pwM(int a, int b, int m){int res(1);while(b){if(b&1)res=(ll)res*a%m;a=(ll)a*a%m;b>>=1;}return res;}
 
 const int infi = 2147483647;
 const ll infl = 9223372036854775807ll;
 const db PI = 3.14159265358979323846;
-
+const int inf = 1000000000+100;
 /*
 struct E{
 	int v, w, n;
-}G[N];
+}G[N*2];
 int cnt, point[N];
-void adde(int u, int v, int w = 0){
+void add_d(int u, int v, int w = 0){
+	G[++cnt]=(E){v,w,point[u]},point[u]=cnt;
+}
+void add_u(int u, int v, int w = 0){
 	G[++cnt]=(E){v,w,point[u]},point[u]=cnt;
 	G[++cnt]=(E){u,w,point[v]},point[v]=cnt;
 }
@@ -113,4 +118,3 @@ int main(){
 */
 	return 0;
 }
-
